059621a7e41a739def60afe9f0ffbfce5dec39e9
Tulir Asokan <tulir@maunium.net>
2019-11-26 20:03:19 +0200

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except IOError:
 
 setuptools.setup(
     name="fbchat-asyncio",
-    version="0.3.0rc3",
+    version="0.3.0",
     url="https://github.com/tulir/fbchat-asyncio",
 
     author="Tulir Asokan",
