c29f4104d1f0fa20783c460a5a5c8506a82dda1a
Tulir Asokan <tulir@maunium.net>
2020-06-22 14:20:16 +0300

    Bump version to 0.6.4

--- fbchat/__init__.py
+++ fbchat/__init__.py
@@ -121,7 +121,7 @@ from ._listen import Listener
 
 from ._client import Client
 
-__version__ = "0.6.3"
+__version__ = "0.6.4"
 
 
 from . import _fix_module_metadata
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except IOError:
 
 setuptools.setup(
     name="fbchat-asyncio",
-    version="0.6.3",
+    version="0.6.4",
     url="https://github.com/tulir/fbchat-asyncio",
 
     author="Tulir Asokan",
